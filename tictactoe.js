05a2b363fd57614120527b5b506138ba7f4e6ee8
kroll.anuar92@gmail.com <kroll.anuar92@gmail.com>
2017-10-20 22:58:05 +0900

    adding scanf

--- tictactoe.js
+++ tictactoe.js
@@ -1,25 +1,35 @@
+var scanf = require("scanf")
+var boardState 
 
 var newGame = function newGame(){
 	
-	var boardState = ["_","_","_",
+	var newBoardState = ["_","_","_",
 		          "_","_","_",
 		          "_","_","_",];
-
-	return boardState;
-
-	
-
+	return newboardState;
 }
 
 var jsboard = function printBoard(boardState){
-	
-	
+
 	for(i=1;i<4;i++){
 	console.log( boardState[(i*1)-1] + " " + boardState[(i*2)-1] + " " + boardState[(i*3)-1])
 	}	
 
 
 }
+
+var player
+
+var move = function playerMove(player,number){
+	
+	if( boardState[number] != "X" && boardState[number] != "O" ){
+		boardState[number] = player;
+	}
+
+}
+
 console.log("Welcome to My Tic Tac Toe Game")
+
 jsboard(newGame())
+
 //console.log(newGame())
